5b16f00f93f0f1c03699c0211e8d31e3cc2bf198
Tugoland <87943173+Tugoland@users.noreply.github.com>
2024-11-21 17:03:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
+var username2 = process.env.username2;
+var password2 = process.env.password2;
+var shared_secret2 = process.env.shared2;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games2 = [730, 440, 	1289310];  // Enter here AppIDs of the needed games
+var status2 = 7;  // 1 - online, 7 - invisible
 
 
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
+user2 = new steamUser();
+user2.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
+user2.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
 	user.gamesPlayed(games);
